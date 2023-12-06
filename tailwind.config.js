764b514d9c97aa3fea03a2b90c0535444fafedff
Manikanda Ruban <manikanda@techalchemy.co>
2023-12-06 09:03:47 +0530

    inital project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3498db', // Change this to your preferred color
+        secondary: '#2ecc71',
+      },
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
